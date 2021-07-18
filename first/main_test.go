01574bdf75fa0f823e827b1bdce51b92acfb0d5f
Dhananjayan Santhanakrishnan <dhananjayan@xage.com>
2021-07-18 14:33:18 -0700

    correcting headers and adding tests

--- first/main_test.go
+++ first/main_test.go
@@ -1,15 +1,17 @@
-// Copyright 2021 Xage Security, Inc. All rights reserved.
-
 package main
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func Composition(t *testing.T) {
+func Testing_Composition(t *testing.T) {
 	t.Run("Composition Test", func(t *testing.T) {
-
+		assert.Equal(t, "4.5", composition(numberFunc, stringFun)(3))
+		assert.Equal(t, "1.5", composition(numberFunc, stringFun)(1))
+		assert.Equal(t, "0", composition(numberFunc, stringFun)(0))
+		assert.Equal(t, "3", composition(numberFunc, stringFun)(2))
 	})
 }
 
